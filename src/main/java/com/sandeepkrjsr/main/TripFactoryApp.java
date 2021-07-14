a1805c1ae901c6c8ecc6639ecf92c0c97e91c10b
Sandeep Kumar <sandeepkrjsr1@gmail.com>
2021-07-15 01:14:12 +0530

    initial commit

---
+++ src/main/java/com/sandeepkrjsr/main/TripFactoryApp.java
@@ -0,0 +1,13 @@
+package com.sandeepkrjsr.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripFactoryApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripFactoryApp.class, args);
+	}
+
+}
